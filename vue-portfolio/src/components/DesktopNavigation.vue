89274eaa172f38042b9fa9b255a08ac9b78148ac
NikoNiiOS <ryxxal@gmail.com>
2024-06-07 12:48:39 +0200

    Desktop navigation
    Added the header navigation menu for desktop users.

---
+++ vue-portfolio/src/components/DesktopNavigation.vue
@@ -0,0 +1,56 @@
+<script>
+</script>
+
+<template>
+    <nav id="desktop-nav">
+        <div class="logo">Ornata</div>
+        <div>
+            <ul class="nav-links">
+                <li><a href="#about">About</a></li>
+                <li><a href="#experience">Experience</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+nav, .nav-links {
+  display: flex;
+}
+
+nav {
+  justify-content: space-around;
+  align-items: center;
+  height: 17vh;
+}
+
+.nav-links{
+  gap: 2.5rem;
+  list-style: none;
+  font-size: 1.5rem;
+}
+
+a {
+  color: #1e252e;
+  text-decoration: none;
+  text-decoration-color: white;
+}
+
+a:hover{
+  color: grey;
+  text-decoration: underline;
+  text-underline-offset: 1.2rem;
+  text-decoration-color: rgb(181, 181, 181);
+}
+
+.logo {
+  color: #1e252e;
+  font-size: 3rem;
+}
+
+.logo:hover{
+  cursor: default;
+}
+</style>
