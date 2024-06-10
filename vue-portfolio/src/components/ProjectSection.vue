d8f1755b3a2bcdaf1964506027fe9f6d31356c56
NikoNiiOS <ryxxal@gmail.com>
2024-06-10 11:05:20 +0200

    Add project section
    #Additions:
    - Project Section
    
    # Changes
    - Minor refactoring

---
+++ vue-portfolio/src/components/ProjectSection.vue
@@ -0,0 +1,106 @@
+<script setup>
+import Contact from './icons/IconContact.vue'
+</script>
+
+<template>
+<section id="projects">
+    <p class="section__text__p1">Browse my recent</p>
+    <h1 class="title">Projects</h1>
+    <div class="experience-details-container">
+        <div class="about-containers">
+        <div class="details-container color-container projects-container">
+            <div class="article-container">
+                <img 
+                src="../assets/img/projects/pic08.jpg" 
+                alt="Image of project 1"
+                class="project-img"
+                />
+            </div>
+            <h2 class="experience-sub-title project-title">Project One</h2>
+            <div class="btn-container">
+                <button class="btn btn-color-2 project-btn">
+                    Github
+                </button>
+                <button class="btn btn-color-2 project-btn">
+                    Presentation
+                </button>
+            </div>
+        </div>
+        <div class="details-container color-container projects-container">
+            <div class="article-container">
+                <img 
+                src="../assets/img/projects/pic09.jpg" 
+                alt="Image of project 2"
+                class="project-img"
+                />
+            </div>
+            <h2 class="experience-sub-title project-title">Project Two</h2>
+            <div class="btn-container">
+                <button class="btn btn-color-2 project-btn">
+                    Github
+                </button>
+                <button class="btn btn-color-2 project-btn">
+                    Presentation
+                </button>
+            </div>
+        </div>
+        <div class="details-container color-container projects-container">
+            <div class="article-container">
+                <img 
+                src="../assets/img/projects/pic10.jpg" 
+                alt="Image of project 3"
+                class="project-img"
+                />
+            </div>
+            <h2 class="experience-sub-title project-title">Project Three</h2>
+            <div class="btn-container">
+                <button class="btn btn-color-2 project-btn">
+                    Github
+                </button>
+                <button class="btn btn-color-2 project-btn">
+                    Presentation
+                </button>
+            </div>
+        </div>
+    </div>
+    </div>
+    <Contact />
+</section>
+</template>
+
+<style>
+#projects {
+    position: relative;
+}
+
+.color-container {
+    border-color: rgb(163, 163, 163);
+    background: white;
+}
+
+.projects-container:hover {
+    filter: drop-shadow(5px 5px 0.5rem black);
+    text-decoration: underline;
+    transition: all 500ms ease;
+}
+
+.project-img {
+    border-radius: 2rem;
+    width: 90%;
+    height: 90%;
+}
+
+.project-title {
+    margin: 1rem;
+    color: black;
+}
+
+.project-btn {
+    color: black;
+    border-color: rgb(163, 163, 163);
+}
+
+.project-btn:hover {
+    text-decoration: none;
+}
+</style>
